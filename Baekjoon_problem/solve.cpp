4985a9fcb251ae2974b32e8d9630be6ec4db4cf6
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-01 17:38:04 +0900

    백준 10773
    처음의 총 입력 정수의 개수를 입력받은 뒤 정수를 입력받아 0이 있을경우 가장 최근의 0이 아닌 수를 제외하고 총 정수의 합을 구하기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -2,39 +2,31 @@
 using namespace std;
 
 int main() {
-    int Tast_Case;
-    cin >> Tast_Case;  // 테스트 케이스 수 입력
-    int* cnt = new int[Tast_Case] {};  // 각 테스트 케이스에서 중복된 좌석 수를 저장할 배열
-    int i, j, k;
+    int int_num, result = 0;  // 입력받을 정수의 개수와 결과값(합계)을 저장하는 변수
+    cin >> int_num;  // 정수의 개수 입력
+    int* integer = new int[int_num];  // 입력받은 정수를 저장할 동적 배열 할당
 
-    for (i = 0; i < Tast_Case; i++) {
-        int player, seat;
-        cin >> player >> seat;  // 각 테스트 케이스에서 플레이어 수와 좌석 수 입력
-        int* pos = new int[player];  // 각 플레이어가 앉을 좌석 번호를 저장할 배열
-
-        // 플레이어가 선택한 좌석 번호 입력
-        for (j = 0; j < player; j++)
-            cin >> pos[j];
-
-        // 좌석 번호 중복 검사
-        for (j = 0; j < player; j++) {
-            for (k = j + 1; k < player; k++) {
-                // 중복된 좌석 발견 시
-                if (pos[j] == pos[k] && pos[j] != 0) {
-                    pos[j] = 0;  // 중복된 좌석 번호를 0으로 설정하여 처리
-                    cnt[i]++;  // 해당 테스트 케이스에서 중복된 좌석 카운트를 증가
+    // 정수를 입력받고, 입력된 값이 0일 경우 이전 값을 찾아 0으로 만듦
+    for (int i = 0; i < int_num; i++) {
+        cin >> integer[i];  // 정수 입력
+        if (integer[i] == 0) {  // 만약 입력된 값이 0이라면
+            for (int j = i - 1; j > -1; j--) {  // 이전 값들을 역순으로 탐색
+                if (integer[j] != 0) {  // 0이 아닌 값을 찾으면
+                    integer[j] = 0;  // 그 값을 0으로 만듦
+                    break;  // 하나만 처리하고 반복 종료
                 }
             }
         }
-
-        delete[] pos;  // 동적 배열 메모리 해제
     }
 
-    // 각 테스트 케이스에서 중복된 좌석 수 출력
-    for (i = 0; i < Tast_Case; i++)
-        cout << cnt[i] << endl;
+    // 배열의 값들을 모두 더하여 결과 계산
+    for (int k = 0; k < int_num; k++)
+        result += integer[k];  // 0이 아닌 값들을 합산
 
-    delete[] cnt;  // 동적으로 할당된 cnt 배열 메모리 해제
+    cout << result << endl;  // 결과 출력
+
+    // 동적으로 할당한 배열 메모리 해제
+    delete[] integer;
 
     return 0;
 }
