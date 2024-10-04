118af9cd398aff77a4d2511ef72c53649d92d0d6
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-04 17:10:39 +0900

    백준 3040번
    9명의 난쟁이는 각각 숫자가 주어져있는데 그중 7명의 합이 100이 되는 7명의 난쟁이를 찾아 그 숫자를 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -2,31 +2,48 @@
 using namespace std;
 
 int main() {
-    int int_num, result = 0;  // 입력받을 정수의 개수와 결과값(합계)을 저장하는 변수
-    cin >> int_num;  // 정수의 개수 입력
-    int* integer = new int[int_num];  // 입력받은 정수를 저장할 동적 배열 할당
+    int small_man[9];
+    int i, j, sum = 0;
+    int pass_num[2] = {0, 1};
+    bool change = false;
 
-    // 정수를 입력받고, 입력된 값이 0일 경우 이전 값을 찾아 0으로 만듦
-    for (int i = 0; i < int_num; i++) {
-        cin >> integer[i];  // 정수 입력
-        if (integer[i] == 0) {  // 만약 입력된 값이 0이라면
-            for (int j = i - 1; j > -1; j--) {  // 이전 값들을 역순으로 탐색
-                if (integer[j] != 0) {  // 0이 아닌 값을 찾으면
-                    integer[j] = 0;  // 그 값을 0으로 만듦
-                    break;  // 하나만 처리하고 반복 종료
-                }
+    for (i = 0; i < 9; i++)
+        cin >> small_man[i];
+
+    while (1) {
+        sum = 0;
+        for (j = 0; j < 9; j++) {
+            if (j != pass_num[0] && j != pass_num[1]) {
+                sum += small_man[j];
             }
         }
+        if (sum != 100) {
+            if (!change) {
+                pass_num[1]++;
+            }
+            else {
+                pass_num[0]++;
+                pass_num[1] = pass_num[0] + 1;
+                change = false;
+            }
+        }
+        else {
+            small_man[pass_num[0]] = 0;
+            small_man[pass_num[1]] = 0;
+            break;
+        }
+
+        if (pass_num[1] > 7)
+            change = true;
+
+        if (pass_num[0] > 6)
+            break;
     }
 
-    // 배열의 값들을 모두 더하여 결과 계산
-    for (int k = 0; k < int_num; k++)
-        result += integer[k];  // 0이 아닌 값들을 합산
-
-    cout << result << endl;  // 결과 출력
-
-    // 동적으로 할당한 배열 메모리 해제
-    delete[] integer;
+    for (i = 0; i < 9; i++) {
+        if(small_man[i] != 0)
+            cout << small_man[i] << endl;
+    }
 
     return 0;
 }
