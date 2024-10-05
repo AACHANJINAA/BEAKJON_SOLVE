b2dbe24415d230ffab6297c6c9953badfed60989
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-05 13:22:58 +0900

    백준 10809
    입력 받은 문자열의 알파벳이 배열의 어느 위치에 있는 지 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -2,48 +2,27 @@
 using namespace std;
 
 int main() {
-    int small_man[9];
-    int i, j, sum = 0;
-    int pass_num[2] = {0, 1};
-    bool change = false;
+    string s; // 문자열을 저장할 변수
+    int str_num[26]; // 알파벳 26개의 위치를 저장할 배열 (a~z)
+    int i; // 반복문에 사용할 변수
 
-    for (i = 0; i < 9; i++)
-        cin >> small_man[i];
+    // 배열 초기화: 모든 알파벳의 위치를 -1로 초기화
+    for (i = 0; i < 26; i++)
+        str_num[i] = -1;
 
-    while (1) {
-        sum = 0;
-        for (j = 0; j < 9; j++) {
-            if (j != pass_num[0] && j != pass_num[1]) {
-                sum += small_man[j];
-            }
-        }
-        if (sum != 100) {
-            if (!change) {
-                pass_num[1]++;
-            }
-            else {
-                pass_num[0]++;
-                pass_num[1] = pass_num[0] + 1;
-                change = false;
-            }
-        }
-        else {
-            small_man[pass_num[0]] = 0;
-            small_man[pass_num[1]] = 0;
-            break;
-        }
+    // 문자열 입력
+    cin >> s;
 
-        if (pass_num[1] > 7)
-            change = true;
-
-        if (pass_num[0] > 6)
-            break;
+    // 문자열의 각 문자를 순차적으로 탐색
+    for (i = 0; i < s.length(); i++) {
+        int index = s[i] - 97; // 문자의 ASCII 값에서 'a'의 ASCII 값(97)을 뺌으로써 알파벳의 위치 계산
+        if (str_num[index] == -1) // 해당 문자가 처음 등장했을 때만 그 위치를 저장
+            str_num[index] = i;
     }
 
-    for (i = 0; i < 9; i++) {
-        if(small_man[i] != 0)
-            cout << small_man[i] << endl;
-    }
+    // 결과 출력: 알파벳 순서대로 각 문자의 첫 등장 위치를 출력
+    for (i = 0; i < 26; i++)
+        cout << str_num[i] << " ";
 
     return 0;
 }
