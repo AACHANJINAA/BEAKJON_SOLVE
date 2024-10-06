5dd596cb1c1a28de1b42983a80c807751909e206
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-06 17:49:20 +0900

    백준 2902번
    KMP

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -2,31 +2,18 @@
 using namespace std;
 
 int main() {
-	int size;
-	cin >> size;
-	int i, j;
-	char** s = new char*[size];
-	int* sum = new int[size];
-	for (i = 0; i < size; i++) {
-		s[i] = new char[3];
-	}
+    string s;
+    cin >> s; // 입력받은 문자열
 
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < 3; j++) {
-			cin >> s[i][j];
-		}
-		sum[i] = (s[i][0] - 48) + (s[i][2] - 48);
-	}
+    cout << s[0]; // 첫 번째 글자는 바로 출력 (약어의 첫 글자)
 
-	for (i = 0; i < size; i++) {
-		cout << sum[i] << endl;
-	}
+    // 두 번째 글자부터 끝까지 반복하면서 '-' 다음 글자 출력
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i - 1] == '-') { // 이전 문자가 '-'이면 현재 문자가 약어의 첫 글자
+            cout << s[i];
+        }
+    }
 
-
-	for (i = 0; i < size; i++)
-		delete[] s[i];
-	delete[] s;
-	delete[] sum;
-
-	return 0;
+    cout << endl; // 결과 출력 후 줄 바꿈
+    return 0;
 }
