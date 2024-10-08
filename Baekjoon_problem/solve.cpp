42b96f20bee8d1485095f2631d577e0d3a51f798
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-08 15:54:17 +0900

    백준 10808번
    알파벳의 개수 세기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
 	string s;
 	cin >> s;
-	int cnt = 1;
+	int i, alphabet[26]{};
 
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] == ',') {
-			cnt++;
-		}
-	}
+	for (i = 0; i < s.length(); i++) 
+		alphabet[s[i] - 97]++;
 
-	cout << cnt;
+	for (i = 0; i < 26; i++)
+		cout << alphabet[i] << ' ';
 
 	return 0;
 }
