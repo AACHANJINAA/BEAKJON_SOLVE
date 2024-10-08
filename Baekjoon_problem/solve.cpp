befa6a979a2a6014e8b45f2f7b201d266ce9814d
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-09 00:15:06 +0900

    백준 11170
    숫자의 범위를 입력 받아 그 숫자들에 ‘0’이 얼마나 들어가 있는지 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,45 +1,45 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <algorithm>
 using namespace std;
-using std::cout;
 
 int main() {
-    string password, text;   // password: 모든 입력을 저장할 문자열, text: 한 줄씩 입력받을 문자열
-    int i, last_change_num = 0;   // i: 반복문 인덱스, last_change_num: 마지막으로 뒤집은 부분의 시작 위치
+    int size, i, j;
+    cin >> size;   // 테스트 케이스의 개수를 입력받음
 
-    while (true) {
-        getline(cin, text);   // 한 줄씩 입력받음
-
-        if (text == "END")   // 입력이 "END"이면 입력 종료
-            break;
-
-        password += text + "\n";   // 입력받은 문자열에 줄바꿈을 추가하고 password에 저장
+    // 2차원 배열을 동적으로 할당 (각 테스트 케이스마다 두 개의 정수 입력받기 위해)
+    int** test_case = new int* [size];
+    for (i = 0; i < size; i++) {
+        test_case[i] = new int[2];   // 각 테스트 케이스마다 2개의 정수 배열 할당 (범위 시작과 끝)
     }
 
-    // password 내의 각 줄을 뒤집는 로직
-    for (i = 0; i < password.length(); i++) {
-        if (password[i] == '\n') {   // 줄바꿈 문자를 만나면 그 앞의 문자열을 뒤집음
-            if (last_change_num == 0) {
-                // 첫 번째 줄을 뒤집음
-                reverse(password.begin(), password.begin() + i);
-            }
-            else if (password[i] == password.length()) {
-                // 마지막 줄을 뒤집음 (비어 있는 마지막 줄을 처리할 때)
-                reverse(password.begin() + last_change_num, password.end());
-            }
-            else {
-                // 중간 줄을 뒤집음
-                reverse(password.begin() + last_change_num, password.begin() + i);
-            }
-
-            last_change_num = i + 1;   // 다음 줄의 시작 위치를 저장
+    // 각 테스트 케이스의 범위 입력
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < 2; j++) {
+            cin >> test_case[i][j];   // 테스트 케이스의 두 값을 입력받음 (범위 시작값, 끝값)
         }
     }
 
-    // 마지막에 추가된 \n을 제거하고 출력
-    for (i = 0; i < password.length() - 1; i++)
-        cout << password[i];
+    string zero_str;   // 숫자를 문자열로 저장할 변수
+
+    // 각 테스트 케이스에 대해 처리
+    for (i = 0; i < size; i++) {
+        // test_case[i][0]에서 test_case[i][1]까지의 숫자를 문자열로 변환하여 zero_str에 저장
+        for (j = test_case[i][0]; j <= test_case[i][1]; j++) {
+            zero_str += to_string(j);   // 숫자를 문자열로 변환해 zero_str에 추가
+        }
+
+        // zero_str에서 '0'의 개수를 세고 출력
+        cout << count(zero_str.begin(), zero_str.end(), '0') << endl;
+
+        // 다음 테스트 케이스를 위해 zero_str을 초기화
+        zero_str = {};
+    }
+
+    // 동적으로 할당한 메모리 해제
+    for (i = 0; i < size; i++)
+        delete[] test_case[i];   // 각 테스트 케이스에 할당된 메모리 해제
+    delete[] test_case;          // test_case 자체에 할당된 메모리 해제
 
     return 0;
 }
