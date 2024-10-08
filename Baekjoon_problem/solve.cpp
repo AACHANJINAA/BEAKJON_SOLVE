21c11581ea2d7fcab97fdbd4c7d2f9ba529ae1dc
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-08 15:50:20 +0900

    백준 10821
    정수의 개수 사기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -5,20 +5,15 @@ using namespace std;
 int main() {
 	string s;
 	cin >> s;
-	int n = s.length() / 10 + 1, i;
-	string* a = new string[n];
+	int cnt = 1;
 
-	for (i = 0; i < n; i++) {
-		if (i != n - 1)
-			a[i] = s.substr(i * 10, 10);
-		else
-			a[i] = s.substr(i * 10);
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == ',') {
+			cnt++;
+		}
 	}
 
-	for (i = 0; i < n; i++)
-		cout << a[i] << endl;
-
-	delete[] a;
+	cout << cnt;
 
 	return 0;
 }
