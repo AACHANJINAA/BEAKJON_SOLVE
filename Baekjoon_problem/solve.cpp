4b3128a8376185c7a63f8c21eedcb130faf4e995
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-08 15:15:56 +0900

    백준 4458
    첫 글자를 대문자로 만들기 / 단, 공백을 포함한 입력받기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	char ascll;
-	cin >> ascll;
+	int size;
+	cin >> size;
+	cin.ignore();
+	string* s = new string[size];
+	int i;
 
-	cout << int(ascll) << endl;
+	for (i = 0; i < size; i++) {
+		getline(cin, s[i]);
+	}
+
+	for (i = 0; i < size; i++) {
+		if (s[i][0] - 97 >= 0)
+			s[i][0] -= 32;
+	}
+
+	for (i = 0; i < size; i++) {
+		cout << s[i] << endl;
+	}
+
+	delete[] s;
 
 	return 0;
 }
