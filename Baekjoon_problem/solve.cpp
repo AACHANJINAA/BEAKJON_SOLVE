2d07c9447a25c2c86f2c6a06bab4bb56571708b9
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-08 23:44:38 +0900

    백준 11365번
    문자를 테스트 케이스 없이 입력받아 \n를 기준으로 문장 뒤집어 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
+using std::cout;
 
 int main() {
-    int size, i;
-    cin >> size;
-    string* s = new string[size];
-    for (i = 0; i < size; i++) {
-        cin >> s[i];
+    string password, text;   // password: 모든 입력을 저장할 문자열, text: 한 줄씩 입력받을 문자열
+    int i, last_change_num = 0;   // i: 반복문 인덱스, last_change_num: 마지막으로 뒤집은 부분의 시작 위치
+
+    while (true) {
+        getline(cin, text);   // 한 줄씩 입력받음
+
+        if (text == "END")   // 입력이 "END"이면 입력 종료
+            break;
+
+        password += text + "\n";   // 입력받은 문자열에 줄바꿈을 추가하고 password에 저장
     }
 
-    for (i = 0; i < size; i++) {
-        if (s[i].length() > 1) {
-            cout << s[i][0] << s[i][s[i].length() - 1] << endl;
-        }
-        else {
-            cout << s[i][0] << s[i][0] << endl;
+    // password 내의 각 줄을 뒤집는 로직
+    for (i = 0; i < password.length(); i++) {
+        if (password[i] == '\n') {   // 줄바꿈 문자를 만나면 그 앞의 문자열을 뒤집음
+            if (last_change_num == 0) {
+                // 첫 번째 줄을 뒤집음
+                reverse(password.begin(), password.begin() + i);
+            }
+            else if (password[i] == password.length()) {
+                // 마지막 줄을 뒤집음 (비어 있는 마지막 줄을 처리할 때)
+                reverse(password.begin() + last_change_num, password.end());
+            }
+            else {
+                // 중간 줄을 뒤집음
+                reverse(password.begin() + last_change_num, password.begin() + i);
+            }
+
+            last_change_num = i + 1;   // 다음 줄의 시작 위치를 저장
         }
     }
+
+    // 마지막에 추가된 \n을 제거하고 출력
+    for (i = 0; i < password.length() - 1; i++)
+        cout << password[i];
 
     return 0;
 }
