05d3acdd4ad51a655f634841b817581c49a7f01f
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-17 16:27:10 +0900

    백쥰 1977번
    완전제곱수 찾기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,77 +1,27 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-class STACK {
-private:
-    int t;
-public:
-    int data[10000]{};
-    STACK() { t = -1; };
-    void push(int x);
-    int pop();
-    int size();
-    int empty();
-    int top();
-};
-
 int main() {
-    int size;
-    cin >> size;
-    STACK S;
+	int m, n;
+	int small_num = -1, sum = 0;
+	cin >> m >> n;
 
-    for (int i = 0; i < size; i++) {
-        string command;
-        cin >> command;
-        if (command == "push") {
-            int n;
-            cin >> n;
-            S.push(n);
-        }
-        else if (command == "pop") {
-            cout << S.pop() << endl;
-        }
-        else if (command == "size") {
-            cout << S.size() << endl;
-        }
-        else if (command == "empty") {
-            cout << S.empty() << endl;
-        }
-        else if (command == "top") {
-            cout << S.top() << endl;
-        }
-    }
+	for (int i = m; i <= m + (n - m); i++) {
+		int k = sqrt(i);
+		if (k * k == i) {
+			if (small_num == -1) {
+				small_num = i;
+			}
+			else if (small_num > i) {
+				small_num = i;
+			}
+			sum += i;
+		}
+	}
 
-    return 0;
-}
-
-void STACK::push(int x) {
-    t++;
-    data[t] = x;
-}
-int STACK::pop() {
-    if (empty() == 1) {
-        return -1;
-    }
-    else {
-        return data[t--];
-    }
-}
-int STACK::size() {
-    return t + 1;
-}
-int STACK::empty() {
-    if (t == -1) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-int STACK::top() {
-    if (empty() == 1) {
-        return -1;
-    }
-    else {
-        return data[t];
-    }
+	if (small_num != -1)
+		cout << sum << endl << small_num;
+	else
+		cout << small_num;
 }
