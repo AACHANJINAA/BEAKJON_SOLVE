f767d427642a0fac4ead99e504871c919b9990a2
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-18 15:43:47 +0900

    백준 5635번
    이름 생일을 입력받아 최고령, 최연소 구하기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
+
+class STUDENT {
+public:
+	string name;
+	string year, month, day;
+};
 
 int main() {
 	int test_case;
 	cin >> test_case;
-	vector<string> result_name(test_case);
-	vector<int> result_price(test_case);
-
-	for (int i = 0; i < test_case; i++) {
-		int p;
-		cin >> p;
-		vector<string> name(p);
-		vector<int> price(p);
-		for (int j = 0; j < p; j++) {
-			cin >> price[j];
-			cin >> name[j];
-
-			if (i == 0) {
-				result_name[i] = name[j];
-				result_price[i] = price[j];
-			}
-			else {
-				if (result_price[i] < price[j]) {
-					result_name[i] = name[j];
-					result_price[i] = price[j];
-				}
-			}
+	vector<STUDENT> s(test_case);
+	vector<string> birthday(test_case);
+	vector<int> old(test_case);
+	int i, j;
+	string young_man, old_man;
+	for (i = 0; i < test_case; i++) {
+		cin >> s[i].name;
+		cin >> s[i].day >> s[i].month >> s[i].year;
+		if (stoi(s[i].month) < 10 && stoi(s[i].day) < 10) {
+			birthday[i] = s[i].year + '0' + s[i].month + '0' + s[i].day;
 		}
+		else if (stoi(s[i].month) < 10) {
+			birthday[i] = s[i].year + '0' + s[i].month + s[i].day;
+		}
+		else if (stoi(s[i].day) < 10) {
+			birthday[i] = s[i].year +  s[i].month + '0' + s[i].day;
+		}
+		else {
+			birthday[i] = s[i].year + s[i].month + s[i].day;
+		}
+		old[i] = stoi(birthday[i]);
+	}
+	
+	sort(old.begin(), old.end());
+
+	for (i = 0; i < test_case; i++) {
+		if (old[0] == stoi(birthday[i]))
+			old_man = s[i].name;
+		if (old[test_case - 1] == stoi(birthday[i]))
+			young_man = s[i].name;
 	}
 
-	for (int i = 0; i < test_case; i++) {
-		cout << result_name[i] << endl;
-	}
+	cout << young_man << endl << old_man;
 }
