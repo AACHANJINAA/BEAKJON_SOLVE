d8891079d93b479a2ec1cf8820f0692f1f734eec
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-20 16:32:14 +0900

    백준 2010번
    멀티탭 연결연결

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -3,26 +3,19 @@
 using namespace std;
 
 int main() {
-	int tc;
-	cin >> tc;
-	vector<int> s(tc);
-	vector<int> n(tc);
-	vector<vector<int>> q(tc);
-	vector<vector<int>> p(tc);
-	vector<int> result(tc, 0);
-	for (int i = 0; i < tc; i++) {
-		cin >> s[i] >> n[i];
-		result[i] += s[i];
-		q[i].resize(n[i]);
-		p[i].resize(n[i]);
-		for (int j = 0; j < n[i]; j++) {
-			cin >> q[i][j] >> p[i][j];
-			result[i] += q[i][j] * p[i][j];
-		}
-	}
+	int N;
+	cin >> N;
+	vector<int> computer(N);
+	int result = 0;
 
-	for (int i = 0; i < tc; i++)
-		cout << result[i];
+	for (int i = 0; i < N; i++) {
+		cin >> computer[i];
+		result += computer[i];
+	}
+	
+	result -= N - 1;
+
+	cout << result;
 
 	return 0;
 }
