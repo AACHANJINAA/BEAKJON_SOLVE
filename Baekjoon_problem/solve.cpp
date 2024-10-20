67112c5feeb9e61b5b000a82f5bcd6a5a6484e12
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-20 15:11:08 +0900

    백준 10833번
    나눠주고 남는 사과 수 구하기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
 using namespace std;
 
 int main() {
-	int test_case, i, j;
-	cin >> test_case;
-	vector<int> term(test_case);
-	vector<vector<int>> credit(test_case);
-	vector<vector<double>> point(test_case);
+	int tc;
+	cin >> tc;
+	vector<int> student(tc);
+	vector<int> apples(tc);
+	int left_apples = 0;
 
-	for (i = 0; i < test_case; i++) {
-		cin >> term[i];
-		credit[i].resize(term[i]);
-		point[i].resize(term[i]);
-		for (j = 0; j < term[i]; j++) {
-			cin >> credit[i][j] >> point[i][j];
+	for (int i = 0; i < tc; i++) {
+		cin >> student[i] >> apples[i];
+		
+		if (student[i] <= apples[i]) {
+			left_apples += apples[i] % student[i];
+		}
+		else {
+			left_apples += apples[i];
 		}
 	}
 
-	cout << fixed;
-	for (i = 0; i < test_case; i++) {
-		int i_sum = 0;
-		double d_sum = 0;
-		for (j = 0; j < term[i]; j++) {
-			i_sum += credit[i][j];
-			d_sum += point[i][j] * credit[i][j];
-		}
-		d_sum /= i_sum;
-		cout << i_sum << ' ' << setprecision(1) << d_sum << endl;
-	}
-
+	cout << left_apples;
 	return 0;
 }
