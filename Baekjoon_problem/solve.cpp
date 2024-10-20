9681b65aa44e61b19009792f554fb12e677b7302
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-20 16:42:58 +0900

    백준 9295번
    주사위 문제

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -5,17 +5,16 @@ using namespace std;
 int main() {
 	int N, i;
 	cin >> N;
-	vector<int> candy(N);
-	vector<int> brother(N);
+	vector<int> first_dice(N);
+	vector<int> second_dice(N);
 
 	for (i = 0; i < N; i++) {
-		cin >> candy[i] >> brother[i];
+		cin >> first_dice[i] >> second_dice[i];
 	}
 
 	for (i = 0; i < N; i++) {
-		printf("You get %d piece(s) and your dad gets %d piece(s).",
-			candy[i] / brother[i], candy[i] % brother[i]);
-		cout << endl;
+		cout << "Case " << i << ": " << first_dice[i] + second_dice[i] << endl;
+
 	}
 
 	return 0;
