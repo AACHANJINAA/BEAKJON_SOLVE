881406d73836f5d1a4f5ad8337ee283050014932
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-20 16:11:53 +0900

    백준 9325번
    변수가 많음
    테스트 케이스, 자동차 가격, 옵션 개수, 각 옵션의 추가 개수와 가격

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -5,21 +5,24 @@ using namespace std;
 int main() {
 	int tc;
 	cin >> tc;
-	vector<int> student(tc);
-	vector<int> apples(tc);
-	int left_apples = 0;
-
+	vector<int> s(tc);
+	vector<int> n(tc);
+	vector<vector<int>> q(tc);
+	vector<vector<int>> p(tc);
+	vector<int> result(tc, 0);
 	for (int i = 0; i < tc; i++) {
-		cin >> student[i] >> apples[i];
-		
-		if (student[i] <= apples[i]) {
-			left_apples += apples[i] % student[i];
-		}
-		else {
-			left_apples += apples[i];
+		cin >> s[i] >> n[i];
+		result[i] += s[i];
+		q[i].resize(n[i]);
+		p[i].resize(n[i]);
+		for (int j = 0; j < n[i]; j++) {
+			cin >> q[i][j] >> p[i][j];
+			result[i] += q[i][j] * p[i][j];
 		}
 	}
 
-	cout << left_apples;
+	for (int i = 0; i < tc; i++)
+		cout << result[i];
+
 	return 0;
 }
