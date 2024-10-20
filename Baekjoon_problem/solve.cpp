2625c602400cfa4462cf198bf06cc06c5cb3701a
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-20 16:39:30 +0900

    백준 10178번
    할로윈 사탕 준거 남은거

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 int main() {
-	vector<int> game(5);
-	int result = 0;
+	int N, i;
+	cin >> N;
+	vector<int> candy(N);
+	vector<int> brother(N);
 
-	for (int i = 0; i < 5; i++)
-		result += game[i];
+	for (i = 0; i < N; i++) {
+		cin >> candy[i] >> brother[i];
+	}
 
-	cout << result;
+	for (i = 0; i < N; i++) {
+		printf("You get %d piece(s) and your dad gets %d piece(s).",
+			candy[i] / brother[i], candy[i] % brother[i]);
+		cout << endl;
+	}
 
 	return 0;
 }
