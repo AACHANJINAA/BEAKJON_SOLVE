2e828c0f7cc646fa5234c2cc710fb979da865e09
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-20 14:46:45 +0900

    백준 10984번
    학점 구하기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 int main() {
-	vector<int> a, b;
+	int test_case, i, j;
+	cin >> test_case;
+	vector<int> term(test_case);
+	vector<vector<int>> credit(test_case);
+	vector<vector<double>> point(test_case);
 
-	while(true) {
-		int n, k;
-		cin >> n >> k;
-		if (n == 0 && k == 0) {
-			break;
-		}
-		else {
-			a.push_back(n);
-			b.push_back(k);
+	for (i = 0; i < test_case; i++) {
+		cin >> term[i];
+		credit[i].resize(term[i]);
+		point[i].resize(term[i]);
+		for (j = 0; j < term[i]; j++) {
+			cin >> credit[i][j] >> point[i][j];
 		}
 	}
 
-	for (int i = 0; i < size(a); i++) {
-		cout << a[i] + b[i];
+	cout << fixed;
+	for (i = 0; i < test_case; i++) {
+		int i_sum = 0;
+		double d_sum = 0;
+		for (j = 0; j < term[i]; j++) {
+			i_sum += credit[i][j];
+			d_sum += point[i][j] * credit[i][j];
+		}
+		d_sum /= i_sum;
+		cout << i_sum << ' ' << setprecision(1) << d_sum << endl;
 	}
+
+	return 0;
 }
