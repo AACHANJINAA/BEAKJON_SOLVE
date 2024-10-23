2198b885b4bbe0b548c5ef8a6f4e8ab81b14c8ab
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-23 18:47:02 +0900

    백준 11944번
    자연수 N, M을 입력 받아 N을 N번 반복하여 출력 단, 너무 길어질 경우 M자리 까지만 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	int a[4][2];
-	int i, j;
-	int human_num = 0, many_people = -1;
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 2; j++) {
-			cin >> a[i][j];
-			if(j == 0)
-				human_num -= a[i][j];
-			else if(j == 1)
-				human_num += a[i][j];
-		}
-		if (human_num > many_people)
-			many_people = human_num;
-	}
-	cout << many_people;
+	int N, M;
+	cin >> N >> M;
+	string result;
+	for (int i = 0; i < N && result.length() < M; i++)
+		result += to_string(N);
+	
+	cout << result.substr(0, M);
+
+	return 0;
 }
