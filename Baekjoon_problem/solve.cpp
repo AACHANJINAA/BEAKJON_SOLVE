3d51d7313fd59496654d39aca6d3aa4d3e01774f
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-23 17:51:52 +0900

    백준 10797번
    첫 줄에 입력 받은 수 와
    둘째 줄에 입력 받은 자연수 5개 중 일치하는 개수 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -2,36 +2,13 @@
 using namespace std;
 
 int main() {
-	int arr[3][4]{};
-	int result[3]{};
-	int i, j;
-	for (i = 0; i < 3; i++){
-		for (j = 0; j < 4; j++) {
-			cin >> arr[i][j];
-			result[i] += arr[i][j];
-		}
+	int n, num[5]{};
+	cin >> n;
+	int i = 0, cnt = 0;
+	for (i = 0; i < 5; i++) {
+		cin >> num[i];
+		if (n == num[i])
+			cnt++;
 	}
-
-	for (i = 0; i < 3; i++) {
-		switch(result[i])
-		{
-		case 0:
-			cout << 'D' << endl;
-			break;
-		case 1:
-			cout << 'C' << endl;
-			break;
-		case 2:
-			cout << 'B' << endl;
-			break;
-		case 3:
-			cout << 'A' << endl;
-			break;
-		case 4:
-			cout << 'E' << endl;
-			break;
-		default:
-			break;
-		}
-	}
+	cout << cnt;
 }
