d8ab19ec62ccb204890f2f094bbc8137554417f5
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-23 17:41:11 +0900

    백준 9085번
    테스트 케이스 수를 입력 받아 각 케이스에 들어가는 자연수의 개수를 입력받은 뒤 자연수를 입력받아 각각의 케이스마다의 자연수의 합을 출력

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
-	int i, a[7]{};
-	vector<int> odd;
-	for (i = 0; i < 7; i++) {
-		cin >> a[i];
-		if (a[i] % 2 == 1)
-			odd.push_back(a[i]);
+	int tc;
+	cin >> tc;
+	vector<int> n(tc);
+	vector<vector<int>> num(tc);
+	vector<int> sum(tc);
+	int i, j;
+	for (i = 0; i < tc; i++) {
+		cin >> n[i];
+		num[i].resize(n[i]);
+		for (j = 0; j < n[i]; j++) {
+			cin >> num[i][j];
+			sum[i] += num[i][j];
+		}
 	}
-	if (!odd.empty()) {
-		int sum = 0;
-		for (i = 0; i < odd.size(); i++)
-			sum += odd[i];
-		sort(odd.begin(), odd.end(), less<>());
-		cout << sum << '\n' << odd[0];
-	}
-	else
-		cout << -1;
+
+	for (i = 0; i < tc; i++)
+		cout << sum[i] << endl;
 }
