7fca0372579cd4b04898338ecbc91b85571b2044
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-24 16:52:56 +0900

    백준 11650번
    테스트 케이스를 입력 받아 (x,y) 좌표를 입력 후 정렬(x 오름차순후 같으면 y 오름차순)
    ㄴ이 문제 시간복잡도 관련 중요함

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-	int N, M;
-	cin >> N >> M;
-	string result;
-	for (int i = 0; i < N && result.length() < M; i++)
-		result += to_string(N);
-	
-	cout << result.substr(0, M);
+	int tc, i;
+	cin >> tc;
+	vector<pair<int, int>> xy(tc);
+	for (i = 0; i < tc; i++) 
+		cin >> xy[i].first >> xy[i].second;
+
+	sort(xy.begin(), xy.end());
+
+	for (i = 0; i < tc; i++)
+		cout << xy[i].first << ' ' << xy[i].second << '\n';
 
 	return 0;
 }
