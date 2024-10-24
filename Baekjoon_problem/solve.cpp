5fb572e40ea5c7e9f686b3a300538f6e85d64a9d
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-24 17:37:20 +0900

    백준 10992번
    테두리만 있는 트리 모양 별찍기

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,51 +1,29 @@
 #include <iostream>
 using namespace std;
 
-string DayOfWeek(int day, int month, int year) {
-	if (month == 1 || month == 2) {
-		month += 12;
-		year -= 1;
-	}
-
-	int two_lower = year % 100;
-	int two_higher = year / 100;
-
-	int DOW = (day + (13 * (month + 1)) / 
-		      5 + two_lower + (two_lower / 4) +
-			  (two_higher / 4) + (two_higher * 5)) % 7;
-
-	switch (DOW)
-	{
-	case 0:
-		return "SAT";
-		break;
-	case 1:
-		return "SUN";
-		break;
-	case 2:
-		return "MON";
-		break;
-	case 3:
-		return "TUE";
-		break;
-	case 4:
-		return "WED";
-		break;
-	case 5:
-		return "THU";
-		break;
-	case 6:
-		return "FRI";
-		break;
-	default:
-		break;
-	}
-}
-
 int main() {
-	int x, y;
-	cin >> x >> y;
+	int size;
+	cin >> size;
+	int i, j;
 
-	cout << DayOfWeek(y, x, 2007);
+	for (i = 1; i <= size; i++) {
+		if (i == size) {
+			for (j = 1; j <= 2 * size - 1; j++) {
+				cout << '*';
+			}
+		}
+		else {
+			for (j = 1; j <= size - i; j++) 
+				cout << ' ';
+			cout << '*';
+			if (i > 1) {
+				for (j = 1; j <= ((2 * (i - 1)) - 1); j++)
+					cout << ' ';
+				cout << '*';
+			}
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
