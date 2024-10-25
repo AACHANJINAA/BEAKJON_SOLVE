d1481dfbd04e87f9958edcf6d372f5848e990fff
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-10-25 16:23:03 +0900

    백준 10828번
    vector로 stack 만들었다

--- Baekjoon_problem/solve.cpp
+++ Baekjoon_problem/solve.cpp
@@ -1,28 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int main() {
-	int size;
-	cin >> size;
-	int i, j;
+int cnt = 0;
 
-	for (i = 1; i <= size; i++) {
-		if (i == size) {
-			for (j = 1; j <= 2 * size - 1; j++) {
-				cout << '*';
-			}
+void push(vector<int>& data, int x) {
+	data.push_back(x);
+	cnt++;
+}
+void pop(vector<int>& data) {
+	if (!data.empty()) {
+		cout << data[cnt - 1] << endl;
+		data.pop_back();
+		cnt--;
+	}
+	else
+		cout << -1 << endl;
+}
+void top(vector<int>& data){
+	if (!data.empty()) {
+		cout << data[cnt - 1] << endl;
+	}
+	else
+		cout << -1 << endl;
+}
+
+int main() {
+	int tc;
+	cin >> tc;
+	vector<string> input(tc);
+	vector<int> data;
+	int i;
+
+	for (i = 0; i < 14; i++) {
+		cin >> input[i];
+		if (input[i] == "push") {
+			int x;
+			cin >> x;
+			push(data, x);
 		}
-		else {
-			for (j = 1; j <= size - i; j++) 
-				cout << ' ';
-			cout << '*';
-			if (i > 1) {
-				for (j = 1; j <= ((2 * (i - 1)) - 1); j++)
-					cout << ' ';
-				cout << '*';
-			}
+		else if (input[i] == "pop") {
+			pop(data);
 		}
-		cout << endl;
+		else if (input[i] == "size") {
+			cout << data.size() << endl;
+		}
+		else if (input[i] == "empty") {
+			if (data.empty())
+				cout << 1 << endl;
+			else
+				cout << 0 << endl;
+		}
+		else if (input[i] == "top") {
+			top(data);
+		}
 	}
 
 	return 0;
