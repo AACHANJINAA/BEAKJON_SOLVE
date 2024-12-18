455e64118df3b39e002c174c69ced85483660533
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2024-12-18 14:50:36 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	
+	int result = 0;
+
+	for (int i = 0; i < n; i++) {
+		result += s[i] - '0';
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
