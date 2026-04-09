2508377ebebcf0523196d5a334bc06956a8d3145
IM CHANJIN <110803149+AACHANJINAA@users.noreply.github.com>
2026-04-09 14:56:55 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 196 ms, Memory: 9268 KB -BaekjoonHub

---
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.cc
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+std::vector<std::vector<int>> graph;
+bool visited[10'0005];
+
+std::vector<int> breath;
+
+struct NODE
+{
+	int node;
+	int depth;
+};
+
+void bfs(int start_node)
+{
+	std::queue<NODE> q;
+	visited[start_node] = true;
+	q.push({start_node, 0});
+
+	while (!q.empty())
+	{
+		NODE nq = q.front();
+		q.pop();
+
+		breath[nq.node] = nq.depth;
+
+		for (int to : graph[nq.node])
+		{
+			if (!visited[to])
+			{
+				visited[to] = true;
+				q.push({to, nq.depth + 1});
+			}
+		}
+	}
+}
+
+int main()
+{
+	int N, M, V;
+	std::cin >> N >> M >> V;
+
+	graph.assign(N + 1, {});
+	breath.resize(N + 1, -1);
+
+	for (int i = 1; i <= M; ++i)
+	{
+		int n1, n2;
+		std::cin >> n1 >> n2;
+		graph[n1].push_back(n2);
+		graph[n2].push_back(n1);
+	}
+
+	for (int i = 1; i <= N; ++i)
+		std::sort(graph[i].begin(), graph[i].end(), [](int n1, int n2)
+			{
+				return n1 > n2;
+			});
+
+	bfs(V);
+
+	for (int n = 1; n <= N; n++) {
+		std::cout << breath[n] << '\n';
+	}
+}
