8818f538f3c71501f2d269255856366b9113aec9
IM CHANJIN <110803149+AACHANJINAA@users.noreply.github.com>
2026-05-25 15:49:57 +0900

    [level 3] Title: 합승 택시 요금, Time: 6.34 ms, Memory: 7.52 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,65 +1,37 @@
-#include <queue>
-#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> dijkstra(int start, const vector<vector<int>>& graph)
-{
-	vector<int> d(graph.size(), 1e9);
-	d[start] = 0;
+// 주의: INF를 1e9로 잡으면 아래 3중 for문에서 1e9 + 1e9 = 2e9가 되어 오버 플로우 위험
+const int INF = 20000000;
 
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.push({ 0, start });
-
-	while (!pq.empty())
-	{
-		int dist = pq.top().first; 
-		int now = pq.top().second; 
-		pq.pop();
-
-		if (d[now] < dist) continue;
-
-		for (int next_node = 1; next_node < graph.size(); ++next_node)
-		{
-			if (graph[now][next_node] == 1e9) continue;
-			int cost = graph[now][next_node];
-			int new_cost = dist + cost;
-			// 거쳐서 가는 것이 기존 최단 거리보다 더 짧다면 갱신
-			if (new_cost < d[next_node])
-			{
-				d[next_node] = new_cost;
-				pq.push({ new_cost, next_node });
-			}
-		}
-	}
-	return d;
-}
-
-// 지점의 개수 n, 출발지점을 나타내는 s, A의 도착지점을 나타내는 a, B의 도착지점을 나타내는 b
-// fares[i] = [c, d, f]는 c-d의 간선 비용이 f
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-	vector<vector<int>> graph(n + 1, vector<int>(n + 1, 1e9));
+    vector<vector<int>> dist(n + 1, vector<int>(n + 1, INF));
 
-    for (const auto& f : fares)
-    {
-		int u = f[0], v = f[1], w = f[2];
-		graph[u][v] = graph[v][u] = w;
+    for (int i = 1; i <= n; i++) dist[i][i] = 0;
+
+    for (const auto& f : fares) {
+        int u = f[0], v = f[1], w = f[2];
+        dist[u][v] = dist[v][u] = w;
     }
 
-	vector<int> dist_s = dijkstra(s, graph);
-	vector<int> dist_a = dijkstra(a, graph);
-	vector<int> dist_b = dijkstra(b, graph);
+    // 플로이드 워셜 알고리즘
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
 
-	int min_time = 2e9;
-	for (int i = 1; i <= n; ++i)
-	{
-		if (dist_s[i] != 1e9 && dist_a[i] != 1e9 && dist_b[i] != 1e9)
-		{
-			int total_time = dist_s[i] + dist_a[i] + dist_b[i];
-			min_time = min(min_time, total_time);
-		}
-	}
+    int min_time = INF;
+    for (int i = 1; i <= n; i++) {
+        if (dist[s][i] != INF && dist[i][a] != INF && dist[i][b] != INF) {
+            int total_time = dist[s][i] + dist[i][a] + dist[i][b];
+            min_time = min(min_time, total_time);
+        }
+    }
 
-	return min_time;
+    return min_time;
 }
